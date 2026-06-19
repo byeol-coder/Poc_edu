3f9a5531db46c33f67281d1fd12ab7f3f3464203
byeol-coder <byeol@dotincorp.com>
2026-06-19 10:25:10 +0900

    Wire partner-ready PoC as review entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
-import './styles.css'
+import PartnerReadyPoC from './PartnerReadyPoC'
+import './partner-ready-poc.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PartnerReadyPoC />
   </StrictMode>,
 )
